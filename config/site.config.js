5425a9a20c50d01dc8d29fa7b1a79f4e765ac81e
xiongcat98 <104610949+xiongcat98@users.noreply.github.com>
2022-04-29 13:05:43 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "熊猫资源社",
+  title: "ONDRIV",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/public',
